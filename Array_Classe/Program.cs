e8ab4c097c284fcce719a49bada709a304d4b5e9
Daniel-afk321 <83432335+Daniel-afk321@users.noreply.github.com>
2021-09-29 21:47:45 -0300

    Adicionar arquivos de projeto.

---
+++ Array_Classe/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Array_Classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Digite o numero de pessoas");
+            //Criando a variável de entrada
+            int A = int.Parse(Console.ReadLine());
+            //Criando vetor com o número da variável de entrada
+            double[] vect = new double[A];
+            //Utilização do comando for para percorrer o vetor
+            for (int i = 0; i < A; i++)
+            {
+                Console.WriteLine("Digite as alturas");
+                //Entrada dos elementos no vetor pelo usuário
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            //Variável soma 
+            double sum = 0.0;
+            //Variável Media
+            double media;
+            //Utilização do for para percorrer o vetor
+            for (int i = 0; i < A; i++)
+                
+            {
+                //Somatoriados elementos inseridos
+                sum += vect[i];
+               
+            }
+            //calculo da media 
+            media = sum / A;
+
+            Console.WriteLine("A Media de altura das pessoas Inseridos é = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
+
